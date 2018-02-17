99f0bf0e61395c9e6ffb36b533eb7ac741b786fa
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2018-02-18 00:51:21 +0100

    remove linting errors for task1
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- task1.py
+++ task1.py
@@ -2,16 +2,18 @@
 # @author tkgroot
 #
 
+
 def main(limit):
-  sum = 0
-  idx = 0
+    sum = 0
+    idx = 0
 
-  while idx<limit:
-    if idx%3 == 0 or idx%5 == 0:
-      sum += idx
-    idx += 1
+    while idx < limit:
+        if idx % 3 == 0 or idx % 5 == 0:
+            sum += idx
+            idx += 1
 
-  return sum
+    return sum
+
 
 if __name__ == '__main__':
-  print(main(1000))
+    print(main(1000))
--- test_task1.py
+++ test_task1.py
@@ -1,4 +1,5 @@
 import task1 as multiply
 
+
 def test_multiply():
-  assert multiply.main(10) == 23
+    assert multiply.main(10) == 23
