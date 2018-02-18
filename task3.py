0bd0b609c23653dca7741e9f60b30f2f14e2d459
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2018-02-18 15:02:39 +0100

    optimized task3
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- task3.py
+++ task3.py
@@ -1,37 +1,36 @@
+# Project Euler Task 3
+# @author tkgroot
 #
-#
-#
-prime = {}
+import math
 
 
 def factor(num):
-  factorArray = []
-  for x in range(3,num):
-    if x%1000000 == 0: print('{} done.'.format(x))
-    if num%x == 0 and isPrime(x):
-      factorArray.append(x)
-  return factorArray
+    factorArray = []
+    for x in range(3, num):
+        if num % x == 0 and isPrime(x):
+            factorArray.append(x)
+    print(factorArray)
+    return factorArray
 
 
 def max_factor(num):
-  return max(factor(num))
+    return max(factor(num))
 
 
 def isPrime(num):
-  try:
-    if prime[num]: return prime[num]
-  except:
-    for x in range(2,num-1):
-      if num%x == 0:
-        prime[num] = False
+    if num % 2 == 0:
         return False
-    prime[num] = True
+    for x in range(3, int(math.sqrt(num)) + 1, 2):
+        if num % x == 0:
+            return False
     return True
 
 
 def main():
-  print(max_factor(600851475143))
+    # print(4 % 2)
+    factor(1475143)
+    # print(max_factor(600851475143))
 
 
 if __name__ == '__main__':
-  main()
+    main()
