2679159db60c18305c3809e2700c555b5fe08c2f
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-05-09 14:53:12 +0200

    move task3 to src directory
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/euler/task3.py
+++ src/euler/task3.py
@@ -1,10 +1,15 @@
-# Project Euler Task 3
-# @author tkgroot
-#
+"""
+Project Euler Task3 - Largest prime factor.
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
 import math
 
 
 def factor(num):
+    """Factorize."""
     factorArray = []
     for x in range(3, num):
         if num % x == 0 and isPrime(x):
@@ -14,10 +19,12 @@ def factor(num):
 
 
 def max_factor(num):
+    """Maximum Factor."""
     return max(factor(num))
 
 
 def isPrime(num):
+    """Determine if number is a prime number."""
     if num % 2 == 0:
         return False
     for x in range(3, int(math.sqrt(num)) + 1, 2):
@@ -27,10 +34,11 @@ def isPrime(num):
 
 
 def main():
+    """main."""
     # print(4 % 2)
     factor(1475143)
     # print(max_factor(600851475143))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
